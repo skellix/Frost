45c02c1333339e4083310984349a9f72be4948df
skellix <24k911@gmail.com>
2014-01-21 21:52:52 -0700

    Added goto label command

--- src/GotoIndexPointer.java
+++ src/GotoIndexPointer.java
@@ -6,7 +6,9 @@ public class GotoIndexPointer extends FrostCommand {
 	public GotoIndexPointer(String name) {
 		this.name = name;
 	}
-
+	public String getInitString() {
+		return this.getClass().getSimpleName()+"(\""+name+"\")";
+	}
 	@Override
 	public void execute(FrostThread frostThread) {
 		frostThread.index.get(frostThread.index.size()-1).set(frostThread.indexes.get(name));
