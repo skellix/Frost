bbabd30ea1e5cef3a27dd895f2be8638fe64efd1
skellix <24k911@gmail.com>
2014-01-21 21:52:36 -0700

    Added localized labels

--- src/IndexPointer.java
+++ src/IndexPointer.java
@@ -6,7 +6,9 @@ public class IndexPointer extends FrostCommand {
 	public IndexPointer(String name) {
 		this.name = name;
 	}
-
+	public String getInitString() {
+		return this.getClass().getSimpleName()+"(\""+name+"\")";
+	}
 	@Override
 	public void execute(FrostThread frostThread) {
 		frostThread.indexes.put(name, frostThread.index.get(frostThread.index.size()-1).get());
